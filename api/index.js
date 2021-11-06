9e944dd5e1b27a21a663d1683e67d655c3cae486
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-11-06 10:47:30 +0100

    feat(.env): add .env file

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
 
 
 
-const listener = app.listen(3000, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('App is listening on port ' + listener.address().port)
 })
