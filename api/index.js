8994e1a6327050ec845013d8a39efd2cb9a6a84c
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-11-06 13:24:32 +0100

    feat(endpoints): add GET '/' endpoint

--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 
-
-
+app.get("/", function (req, res) {
+  res.send("test");
+});
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('App is listening on port ' + listener.address().port)
 })
