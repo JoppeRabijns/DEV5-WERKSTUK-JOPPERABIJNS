df82af47758129f27e0bfd0aa1b59d781b44cccb
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-11-09 16:36:31 +0100

    feat(postgres): drop and create students and meals table

---
+++ config/postegresql.js
@@ -0,0 +1,42 @@
+const knex = require('knex')({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
+  searchPath: ['knex', 'public'],
+});
+
+knex.schema.dropTableIfExists('students');
+knex.schema.dropTableIfExists('meals');
+
+knex.schema.createTable('students', function (table) {
+  table
+    .increments('id')
+    .primary();
+  table
+    .string('name', 100)
+    .notNullable();
+  table
+    .string('email', 100)
+    .notNullable();
+  table
+    .string('password', 100)
+    .notNullable();
+  t.timestamps();
+})
+
+knex.schema.createTable('meals', function (table) {
+  table
+    .increments('id')
+    .primary();
+  table
+    .string('title', 100)
+    .notNullable();
+  table
+    .string('description', 500)
+    .notNullable();
+  table
+    .double('price', 4,2)
+    .notNullable();
+  table.timestamps();
+})
+
+module.exports = knex;
