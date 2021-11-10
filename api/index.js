4e65073122f2b440aba796e1218fcadb893a1fc3
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-11-10 13:11:41 +0100

    docs(endpoints): add documentation to paths

--- api/index.js
+++ api/index.js
@@ -2,14 +2,30 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 
-app.get("api/students", function (req, res) {
+/**
+ * [GET] /api/students
+ * returns all students in database upon get request
+ * @returns {Object} all students in database
+ */
+ app.get("api/students", function (req, res) {
   res.send("get all users");
 });
 
+/**
+ * [PUT] /api/students/:id
+ * 
+ * 
+ * @returns {Object} updated student object
+ */
 app.put("api/students/:id", function (req, res) {
   res.send("update one user");
 });
 
+/**
+ * [DELETE] /api/students/:id
+ * Delete student by id 
+ * @returns HTTP status 204 indicates successful deletion.
+ */
 app.delete("api/students/:id", function (req, res) {
   res.status(204);
 });
