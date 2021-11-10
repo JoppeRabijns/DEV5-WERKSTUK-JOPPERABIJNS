f3f3f4af9c13cd5637dd8f6f681c2129cbf4d6bf
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-11-10 13:10:54 +0100

    feat(endpoints): add PUT and DELETE path

--- api/index.js
+++ api/index.js
@@ -2,9 +2,19 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 
-app.get("/", function (req, res) {
-  res.send("test");
+app.get("api/students", function (req, res) {
+  res.send("get all users");
 });
+
+app.put("api/students/:id", function (req, res) {
+  res.send("update one user");
+});
+
+app.delete("api/students/:id", function (req, res) {
+  res.status(204);
+});
+
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('App is listening on port ' + listener.address().port)
 })
