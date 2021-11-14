8cab80d4980938f7d8f6fb074897d8d7dc4af37b
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-11-14 13:18:57 +0100

    feat(tests): add server.js, add endpoint integration tests

---
+++ __tests__/ integrationTests/integration.test.js
@@ -0,0 +1,49 @@
+const { expect, describe, test } = require("@jest/globals");
+const app = require('../../api/server');
+const supertest = require("supertest");
+const request = supertest(app)
+
+const student =  {id:1,name: "Student1", email: "email@email.be", password:"password123"};
+const studentUpdate =  {name: "Update1", email: "email@email.be", password:"password123"};
+
+describe("test endpoints of api", () => {
+  it("GET all student", (done) => {
+    request
+    .get('/api/students')
+    .expect(200)
+    .end(() => done());
+  });
+
+  it('Create a student', (done) => {
+    request
+    .post('/api/students')
+    .send(student)
+    .expect(200)
+    .end((err, res) => {
+      expect(res.body[0].name).toEqual("Student1")
+      expect(res.body[0].email).toEqual("email@email.be")
+      expect(res.body[0].password).toEqual("password123")
+      done();
+   });
+  })
+  
+  it('Update a student', (done) => {
+    request
+    .put('/api/students/1')
+    .send(studentUpdate)
+    .expect(200)
+    .end((err, res) => {
+      expect(res.body[0].name).toEqual("Update1")
+      expect(res.body[0].email).toEqual("email@email.be")
+      expect(res.body[0].password).toEqual("password123")
+      done();
+   });
+  })
+
+  it('Delete a student', (done) => {
+    request
+    .delete('/api/students/1')
+    .expect(204)
+    .end(() => done());
+  });
+});
