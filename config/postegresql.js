d2e093d8c9e0b20d54a0dc932b5fa0a3e6cc687d
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-12-04 12:06:54 +0100

    feat(postgres:table): add city table

--- config/postegresql.js
+++ config/postegresql.js
@@ -1,41 +1,42 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const knex = require('knex')({
-  client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : "postgres://postgres:test@localhost:5432/postgres",
-  searchPath: ['knex', 'public'],
-  });
+const knex = require("knex")({
+  client: "pg",
+  connection: process.env.PG_CONNECTION_STRING
+    ? process.env.PG_CONNECTION_STRING
+    : "postgres://postgres:test@localhost:5432/postgres",
+  searchPath: ["knex", "public"],
+});
 
 (async () => {
   try {
-    knex.schema.hasTable('students').then(function(exists) {
+    knex.schema.hasTable("city").then(function (exists) {
       if (!exists) {
-        return knex.schema.withSchema('public').createTable('students', function (table) {
-          table
-            .increments('id')
-            .primary();
-          table
-            .string('name', 100)
-            .notNullable();
-          table
-            .string('email', 100)
-            .notNullable();
-          table
-            .string('password', 100)
-            .notNullable();
-          table.timestamps();
-        });
+        return knex.schema
+          .withSchema("public")
+          .createTable("city", function (table) {
+            table.increments("id").primary();
+            table.string("name", 100).notNullable();
+            table.timestamps();
+          });
       }
-    })
+    });
+    knex.schema.hasTable("students").then(function (exists) {
+      if (!exists) {
+        return knex.schema
+          .withSchema("public")
+          .createTable("students", function (table) {
+            table.increments("id").primary();
+            table.string("name", 100).notNullable();
+            table.string("email", 100).notNullable();
+            table.string("password", 100).notNullable();
+            table.timestamps();
+          });
+      }
+    });
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-})()
-
-
-
-
-
-
+})();
 
 module.exports = knex;
